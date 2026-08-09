756ec670ad9950d5925cb7c149f0595dc6098dee
PP6464 <panthhp@outlook.com>
2026-08-09 14:46:09 +0100

    style the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
+import PageContainer from '@/app/components/page-container/page-container';
+
 export default function NotFound() {
   return (
-    <div className={'flex flex-col items-center justify-center min-h-30'}>
+    <PageContainer>
       <h1>Not Found</h1>
       <p>Please use one of the links above to view my portfolio</p>
-    </div>
+    </PageContainer>
   );
 }
