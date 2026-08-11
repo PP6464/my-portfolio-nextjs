c02314e3c367d12c3b48e349f4cdf11c07a7aece
PP6464 <panthhp@outlook.com>
2026-08-11 17:48:30 +0100

    source tag info from firestore

--- app/my-code/page.tsx
+++ app/my-code/page.tsx
@@ -2,13 +2,13 @@
 
 import PageContainer from '@/app/components/page-container/page-container';
 import { useEffect, useState } from 'react';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { collection, DocumentReference, onSnapshot } from 'firebase/firestore';
 import { getDb } from '@/lib/firebase/client';
 import './my-code.css';
 import Link from 'next/link';
 import { ArrowOutward } from '@mui/icons-material';
 
-type Framework = {
+type Tag = {
 	name: string;
 	colour: string;
 	border: string;
@@ -17,9 +17,42 @@ type Framework = {
 type Code = {
 	link: string;
 	name: string;
-	frameworks: Framework[];
+	tags: DocumentReference[];
 	deploy_link?: string;
 	in_progress: boolean;
+}
+
+function TagView({ docRef }: { docRef: DocumentReference }) {
+	const [tag, setTag] = useState<Tag>({
+		name: '',
+		colour: 'var(--background)',
+		border: 'var(--background)',
+	});
+
+	useEffect(() => {
+		return onSnapshot(
+			docRef,
+			(snap) => {
+				setTag(snap.data() as Tag);
+			},
+			(err) => {
+				console.error('Failed to load tag data: ', err);
+			},
+		);
+	}, [docRef]);
+
+	return (
+		<div  style={ {
+			backgroundColor: tag.colour,
+			border: `2px solid ${ tag.border }`,
+			color: tag.border,
+			borderRadius: '50px',
+			width: 'fit-content',
+			padding: '5px 10px',
+		} }>
+			<p>{ tag.name }</p>
+		</div>
+	)
 }
 
 export default function MyCode() {
@@ -32,7 +65,7 @@ export default function MyCode() {
 				setCodes(snap.docs.map((d) => d.data() as Code));
 			},
 			(err) => {
-				console.error('Failed to load exploring areas:', err);
+				console.error('Failed to load exploring areas: ', err);
 			},
 		);
 	}, []);
@@ -44,17 +77,8 @@ export default function MyCode() {
 				<div key={ code.name } className={ 'code' }>
 					<h2>{ code.name }{ code.in_progress ? ' [In Progress]' : '' }</h2>
 					<div>
-						{ code.frameworks.map((framework) => (
-							<div key={ `${ code.name }-${ framework.name }` } style={ {
-								backgroundColor: framework.colour,
-								border: `2px solid ${ framework.border }`,
-								color: framework.border,
-								borderRadius: '50px',
-								width: 'fit-content',
-								padding: '5px 10px',
-							} }>
-								<p>{ framework.name }</p>
-							</div>
+						{ code.tags.map((tag) => (
+							<TagView key={ `${ code.name }-${ tag.id }` } docRef={tag} />
 						)) }
 					</div>
 					<Link target={ '_blank' } rel={ 'noreferrer' } href={ code.link }>
