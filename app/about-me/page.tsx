8792fe98bee001fd2ca8fe552dc66d404035373b
PP6464 <panthhp@outlook.com>
2026-08-16 13:10:07 +0100

    add achievements section

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import PageContainer from '@/app/components/page-container/page-container';
 import { GitHub, LinkedIn, Phone } from '@mui/icons-material';
 import Email from '@mui/icons-material/Email';
 import Link from 'next/link';
 import './about-me.css';
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { getDb } from '@/lib/firebase/client';
+
+type Achievement = {
+	name: string;
+	desc: string;
+}
 
 export default function AboutMe() {
 	const contactDetails = {
@@ -11,6 +21,20 @@ export default function AboutMe() {
 		email: { url: 'mailto:32ppatel@gmail.com', value: '32ppatel@gmail.com', icon: <Email/> },
 		phone: { url: 'tel:+44-73058-21678', value: '+44 73058 21678', icon: <Phone/> },
 	};
+	const [achievements, setAchievements] = useState<Achievement[]>([]);
+
+	useEffect(() => {
+		onSnapshot(
+			collection(getDb(), 'achievements'),
+			(snap) => {
+				setAchievements(snap.docs.map((d) => d.data() as Achievement));
+			},
+			(err) => {
+				alert('Failed to load data');
+				console.error('Failed to load achievements: ', err);
+			},
+		);
+	}, []);
 
 	return (
 		<PageContainer>
@@ -19,6 +43,19 @@ export default function AboutMe() {
 				different frameworks and with many
 				different areas of programming, spanning web and app development to API and neural network development. I am
 				always learning and exploring new frameworks and areas of programming.</p>
+			<div style={ { padding: '10px' } }></div>
+			<h1>Achievements/Prizes</h1>
+			<ul>
+				{ achievements.map(achievement => (
+					<li key={ achievement.name } className={ 'achievement' }>
+						<h3>{ achievement.name }</h3>
+						<p className={ 'info-text' }>{ achievement.desc }</p>
+					</li>
+				)) }
+				{ achievements.length === 0 ? <p>Loading achievements...</p> : <></> }
+			</ul>
+			<div style={ { padding: '10px' } }></div>
+
 			<h1>Contact Details</h1>
 			<ul>
 				<div key={ 'linkedIn' } className={ 'contact-detail' }>
